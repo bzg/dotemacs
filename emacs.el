43336cb2cfff7ae5d947d07137a92cfeef64e390
Bastien <bzg@gnu.org>
2021-05-24 12:04:47 +0200

    Use firefox-trunk

--- emacs.el
+++ emacs.el
@@ -1155,7 +1155,7 @@
 (setq delete-old-versions t)
 
 ;; (setq browse-url-browser-function 'eww-browse-url)
-(setq browse-url-generic-program "firefox")
+(setq browse-url-generic-program "firefox-trunk")
 (setq browse-url-firefox-new-window-is-tab t)
 
 (use-package whitespace
